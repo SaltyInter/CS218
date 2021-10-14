da98293fdf1ff59217c038fadaf088907e8fd3bd
SaltyInter <mshiroma01@gmail.com>
2021-10-14 02:02:08 -0700

    added helper.cpp

--- assn6/assn6.asm
+++ assn6/assn6.asm
@@ -1,10 +1,18 @@
-;	Assignment #5
+; Assignment #6
 ; Author: Matthew Shiroma
+; Name: Matthew Shiroma
 ; Section: 1004
-; Date Last Modified: 10-10-21
-;	This program will explore the use of functions
+; Date Last Modified: 
+; This program
+
+; yasm -g dwarf2 -f elf64 assn6.asm
+; g++ -g -no-pie helper.cpp assn6.o
+; ./a.out
 
 section .data
+;externs
+extern atof, ceil, printBallonsRequired
+
 ;	System Service Constants
 	SYSTEM_EXIT 				equ 60
 	SUCCESS 					equ 0
@@ -18,6 +26,15 @@ section .data
 section .bss
 
 section .text
+
+;Fuction that will check and convert the command line args
+; Arg 1.) rdi - argc 
+; Arg 2.) rsi - argv
+; Arg 3.) rdx - weight value float
+; Arg 4.) rcx - diameter value float
+global proccessCommandLine
+proccessCommandLine:
+
 global main
 main:
 
